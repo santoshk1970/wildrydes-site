d4c7833742d28ceead40c02c0d3ec1979fdaf340
Santosh Kulkarni <kulkarnisg@gmail.com>
2024-02-04 09:30:55 -0600

    Updated config file

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+  cognito: {
+    userPoolId: "us-east-1_2zYUVFuG9", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "65hbrl3j494mmaqrv18vbfv11v", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: "us-east-1", // e.g. us-east-2
+  },
+  api: {
+    invokeUrl: "https://main.d27rbafvyyx6se.amplifyapp.com/", // d',
+  },
+};
--- scripts/config.js
+++
@@ -1,9 +0,0 @@
-var Wildrydes = {
-  config: {
-    region: '',
-    identityPoolId: '',
-    userPoolId: 'us-east-1_2zYUVFuG9',
-    userPoolClientId: '65hbrl3j494mmaqrv18vbfv11v',
-    apiUrl: '',
-  }
-}
