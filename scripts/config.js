de43e3297f0d395e916edb6fc10d7328168b1f9a
Santosh Kulkarni <kulkarnisg@gmail.com>
2024-02-04 08:59:02 -0600

    configuring pool id and client id

---
+++ scripts/config.js
@@ -0,0 +1,9 @@
+var Wildrydes = {
+  config: {
+    region: '',
+    identityPoolId: '',
+    userPoolId: 'us-east-1_2zYUVFuG9',
+    userPoolClientId: '65hbrl3j494mmaqrv18vbfv11v',
+    apiUrl: '',
+  }
+}
